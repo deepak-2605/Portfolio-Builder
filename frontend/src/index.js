e34d1c88275500b9c2061bb2aea49bc8fbd2362e
AnkitGurwan <ankitgurwan083@gmail.com>
2023-09-25 16:39:17 +0530

    Added react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={ store }>
+    <ToastContainer/>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
